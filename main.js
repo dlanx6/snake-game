9b2d39b2fc5268a975aea409d89b0f1f7e166eb0
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2024-09-12 21:11:32 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Canvas
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
+
+// Score
+const score = document.querySelector("#score-number");
+
+// 
+const newGameBtn = document.querySelector("#new-game-button");
